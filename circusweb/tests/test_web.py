35cf6b48663fe1f5422804d2275a54594e788429
Tarek Ziade <tarek@ziade.org>
2013-01-02 17:09:00 +0100

    moar tests

--- circusweb/tests/test_web.py
+++ circusweb/tests/test_web.py
@@ -49,3 +49,19 @@ class TestHttpd(TestCircus):
         # let's follow it
         res = res.follow()
         self.assertTrue('tcp://127.0.0.1:5557' in res.body)
+
+    def test_watcher_page(self):
+        # let's go to the watcher page now
+        watcher_page = self.app.get('/watchers/sleeper')
+        self.assertTrue('<span class="num_process">1</span>' in
+                        watcher_page.body)
+
+        # let's add two watchers
+        self.app.get('/watchers/sleeper/process/incr')
+        self.app.get('/watchers/sleeper/process/incr')
+
+        # let's go back to the watcher page now
+        # and check the number of watchers
+        watcher_page = self.app.get('/watchers/sleeper')
+        self.assertTrue('<span class="num_process">3</span>' in
+                        watcher_page.body)
